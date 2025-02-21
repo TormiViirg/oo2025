52cfa10c57ab7b8eeb477bfd1f3885a90dfe39a9
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-02-22 01:00:33 +0200

    3. tund töötav

--- 01_07.02_andmemudelid/veebipood/src/main/java/ee/tormi/veebipood/controller/OrderController.java
+++ 01_07.02_andmemudelid/veebipood/src/main/java/ee/tormi/veebipood/controller/OrderController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 
 @RestController
@@ -17,13 +17,21 @@ public class OrderController {
     @Autowired
     OrderRepository orderRepository;
 
+    // TODO: Ei tagastataks kõiki tellimusi
+    // TODO: Pean võtma front-endist AINULT ID ja mitte usaldama front-endist tulevad toote hinda
     @PostMapping("orders")
-    public List<Order> addOrder(@RequestBody Order order){
-        order.setCreated(new Date());
+    public List<Order> addOrder(@RequestBody Order order) {
+        order.setCreated(new Date(System.currentTimeMillis()));
         double sum = 0;
         for (Product p: order.getProducts()) {
             sum = sum + p.getPrice();
+            // sum += p.getPrice();
         }
+//        for (int i = 0; i < order.getProducts().size(); i++) {
+//            Product p = order.getProducts().get(i);
+//            sum = sum + p.getPrice();
+////             sum += p.getPrice();
+//        }
         order.setTotalSum(sum);
         orderRepository.save(order);
         return orderRepository.findAll();
