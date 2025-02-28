fe559f4f84463cee9a68a73122c6eb3c1bccf4b0
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-02-28 14:41:00 +0200

    3. libisev

--- 04_28.02_proovikontrolltoo1/proovikt/src/main/java/ee/tormi/proovikt/controller/SuvaController.java
+++ 04_28.02_proovikontrolltoo1/proovikt/src/main/java/ee/tormi/proovikt/controller/SuvaController.java
@@ -5,6 +5,7 @@ import ee.tormi.proovikt.repository.SuvaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 /*Tee lisaks kolm API otspunkti: 1) tagastab numbrina kõikide andmebaasis olevate numbrite summa (täisnumber) 2) tagastab
 kõikide andmebaasis olevate arvude aritmeetilise keskmise (komakohaga) 3) tagastab kõige suurema numbri,
@@ -46,7 +47,31 @@ public class SuvaController {
         return (int) suvad.stream().mapToDouble(Suva::getArvsisend).max().orElse(0.0);
     }
 
-/*Tee lisaks kolm API otspunkti:
+    @GetMapping("libisev")
+    public List<Double> getLibisev() {
+        List<Suva> suvad = suvaRepository.findAll();
+        List<Double> tulemused = new ArrayList<>();
+
+        if (suvad.size() < 3) {
+            throw new RuntimeException("ERROR_FOR_ROLLING_AVERAGE_3_INPUTS_ARE_NEEDED_AT_MINIMUM");
+        }
+
+        for (int i = 0; i < suvad.size() - 2; i++) {
+            double keskmine = (suvad.get(i).getArvsisend() +
+                    suvad.get(i + 1).getArvsisend() +
+                    suvad.get(i + 2).getArvsisend()) / 3;
+            tulemused.add(keskmine);
+        }
+        return tulemused;
+    }
+
+
+/*
+* Koosta API otspunkt andmebaasis olevate arvude libiseva keskmise leidmiseks. Väljundiks on massiiv, mis on andmebaasis
+* olevatest arvudest kahe elemendi võrra lühem ning mille iga elemendi väärtuseks on andmebaasis vastava elemendi
+* ning selle järgmise ja ülejärgmise elemendi keskmine.
+
+Tee lisaks kolm API otspunkti:
 1) tagastab numbrina kõikide andmebaasis olevate numbrite summa (täisnumber)
 2) tagastab kõikide andmebaasis olevate arvude aritmeetilise keskmise (komakohaga)
 3) tagastab kõige suurema numbri, mis andmebaasis leidub (täisnumber)*/
