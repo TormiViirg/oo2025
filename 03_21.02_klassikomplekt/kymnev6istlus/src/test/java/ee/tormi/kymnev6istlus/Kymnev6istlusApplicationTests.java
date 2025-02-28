6298513a2551d124ead5779829fd1275b2ffad73
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-02-28 11:59:35 +0200

    baas

---
+++ 03_21.02_klassikomplekt/kymnev6istlus/src/test/java/ee/tormi/kymnev6istlus/Kymnev6istlusApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.tormi.kymnev6istlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kymnev6istlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
