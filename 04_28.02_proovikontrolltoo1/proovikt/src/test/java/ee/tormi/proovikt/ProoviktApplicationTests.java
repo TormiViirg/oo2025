1d1cb59ab649d0296db41e22a826d79e16eb81ca
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-02-28 12:24:10 +0200

    proovikt baas

---
+++ 04_28.02_proovikontrolltoo1/proovikt/src/test/java/ee/tormi/proovikt/ProoviktApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.tormi.proovikt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProoviktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
