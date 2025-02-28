2939592a1e44cd91b9673001dacddc1fd5341768
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-02-28 14:56:53 +0200

    lisaharjutus baas

---
+++ 04_28.02_proovikontrolltoo1/hulknurk/src/test/java/ee/tormi/hulknurk/HulknurkApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.tormi.hulknurk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulknurkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
