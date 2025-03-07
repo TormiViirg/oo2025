01bed8fdf694b50bd0d1f0f19468dd4d87416962
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2025-03-07 05:52:02 +0200

    too deep now

--- 03_21.02_klassikomplekt/kymnev6istlus/src/main/java/ee/tormi/kymnev6istlus/controller/AthleteController.java
+++ 03_21.02_klassikomplekt/kymnev6istlus/src/main/java/ee/tormi/kymnev6istlus/controller/AthleteController.java
@@ -1,20 +1,36 @@
 package ee.tormi.kymnev6istlus.controller;
 
 import ee.tormi.kymnev6istlus.entity.Athlete;
+import ee.tormi.kymnev6istlus.entity.Results;
 import ee.tormi.kymnev6istlus.repository.AthleteRepository;
+import ee.tormi.kymnev6istlus.repository.ResultsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AthleteController {
 
     @Autowired
     AthleteRepository athleteRepository;
+    ResultsRepository resultsRepository;
+
+    //kuna ma ei taibanud omal elu lihtsaks teha siis kuna sportlane võib mitmel võistlusel osaleda ja seetõtu mitu resulti
+    @GetMapping("athlete/{id}/totalScores")
+    public ResponseEntity<List<Integer>> getTotalScores(@PathVariable Long id) {
+        Athlete athlete = athleteRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+
+        List<Integer> totalScores = athlete.getResults()
+                .stream()
+                .map(Results::getTotalScore)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(totalScores);
+    }
+
 
     @PostMapping("athlete")
     public List<Athlete> addAthlete(@RequestBody Athlete athlete) {
--- 03_21.02_klassikomplekt/kymnev6istlus/src/main/java/ee/tormi/kymnev6istlus/repository/ResultsRepository.java
+++ 03_21.02_klassikomplekt/kymnev6istlus/src/main/java/ee/tormi/kymnev6istlus/repository/ResultsRepository.java
@@ -2,6 +2,10 @@ package ee.tormi.kymnev6istlus.repository;
 
 import ee.tormi.kymnev6istlus.entity.Results;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ResultsRepository extends JpaRepository<Results, Long> {
+   // @Query("SELECT r.totalScore FROM Result r WHERE r.athlete.id = :athleteId")
+    //Integer findTotalScoreByAthleteId(@Param("athleteId") Long athleteId);
 }
